91dab3f807857018cb084f42f28769f2e9249eec
justiceleague535 <35665045+justiceleague535@users.noreply.github.com>
2018-04-01 23:03:56 -0400

    Add files via upload

--- Display Manager.py
+++ Display Manager.py
@@ -6,14 +6,13 @@ from PIL import Image
 from tkinter import * 
 import qrcode
 import image
-
+import webbrowser
 
 class QRCreator:
 
     separator = '0x1D'
     notFound = 'NULL'
     end_of_message = 'EOT'
-    message_header = '[]>'
     null_check = str(-1)
     message = '[]>'
     message_block = ''
@@ -28,7 +27,14 @@ class QRCreator:
         self.serialNumber = DataPlate.serial_number
         niin = str(self.niin)
         self.niin_string = niin[0:9]
-        self.data = [str(self.odometerRead),str(self.fuelEconomy),str(self.fuelLevel1), str(self.engineHours),str(self.fuelUsed),self.niin_string,str(self.serialNumber)]
+        self.headers = ['SN','N','VO','FU','FE','EH','FL']
+        self.ends = ['','','DH','G','DHG','HR','P1']
+        
+        self.data = [str(self.serialNumber),self.niin_string,str(self.odometerRead),str(self.fuelUsed),str(self.fuelEconomy),str(self.engineHours),str(self.fuelLevel1)]
+
+        
+
+        #self.data = [str(self.odometerRead),str(self.fuelEconomy),str(self.fuelLevel1), str(self.engineHours),str(self.fuelUsed),self.niin_string,str(self.serialNumber)]
 
     def display(self):
         print(qrcode.constants.ERROR_CORRECT_H)
@@ -40,16 +46,18 @@ class QRCreator:
 
         for x in range(0, 7):
             length = len(self.data[x])
-            if x != 6:
+            if x != 0:
                 zero_count = 9 - length
+                self.message_block = self.message_block + self.headers[x]
                 for y in range(0, zero_count):
                     self.message_block = self.message_block + '0'
-                self.message_block = self.message_block + self.data[x]
+                self.message_block = self.message_block + self.data[x] + self.ends[x]
             else:
                 zero_count = 6 - length
+                self.message_block = self.message_block + self.headers[x]
                 for y in range(0, zero_count):
                     self.message_block = self.message_block + '0'
-                self.message_block = self.message_block + self.data[x]
+                self.message_block = self.message_block + self.data[x] + self.ends[x]
             self.message = self.message + self.message_block
             if x != 6:
                 self.message = self.message + self.separator
@@ -65,13 +73,16 @@ class QRCreator:
         img = qr.make_image()
 
         img.save("image.jpg")
-        img.show()
+        #img.show()
 
-        data1 = "Vehicle Odometer: " + str(self.odometerRead)
-        data2 = "Total Engine Hours: " + str(self.engineHours)
-        data3 = "Total Fuel Used: " + str(self.fuelUsed)
-        data4 = "Average Fuel Economy: " + str(self.fuelEconomy)
-        data5 = "Fuel Level 1: " + str(self.fuelLevel1)
+        filename = "image.jpg"
+        webbrowser.open(filename)
+
+        data1 = "Vehicle Odometer: " + str(self.odometerRead) + " Miles"
+        data2 = "Total Engine Hours: " + str(self.engineHours) + " Hours"
+        data3 = "Total Fuel Used: " + str(self.fuelUsed) + " Gallons"
+        data4 = "Fuel Economy: " + str(self.fuelEconomy) + " Miles per Gallon"
+        data5 = "Fuel Level 1: " + str(self.fuelLevel1) + " %"
         data6 = "Serial Number: " + str(self.serialNumber)
         data7 = "NIIN: " + self.niin_string
 
@@ -87,6 +98,8 @@ class DataPlate:
 
     serial_number = -1
     niin = -1
+    tamcn = -1
+    test = -1
 
     def __init__(self, filename):
         self.file_name = filename
@@ -102,12 +115,19 @@ class DataPlate:
         
         self.niin = data[3]
 
+        self.tamcn = data[5]
+
+        self.test = data[7]
+        
+
+        print(self.test)
+
     def display(self):
         data1 = "Serial Number: " + str(self.serial_number)
         data2 = "NIIN: " + str(self.niin)
         totalData = data1 + '\n' + data2 
         root = Tk()
-        root.geometry('250x150+1500+300')
+        root.geometry('250x150+1000+100')
         label = Label(root, text= totalData, width=40, height = 50, font=(None,15))
         label.pack()
 
@@ -115,31 +135,44 @@ class DataPlate:
 # Class for human readable data
 class TextDisplay:
 
-    def __init__(self, odometerRead, fuelEconomy, fuelLevel1,engineHours,fuelUsed):
+
+
+    def __init__(self, odometerRead, fuelEconomy, fuelLevel1,engineHours,fuelUsed,plate):
+        self.serial_number = plate.serial_number
+        self.niin = plate.niin
         self.odometerRead = odometerRead
         self.fuelEconomy = fuelEconomy
         self.fuelLevel1 = fuelLevel1
         self.engineHours = engineHours
         self.fuelUsed = fuelUsed
+        self.tamcn = plate.tamcn
+        self.test = plate.test
+        
+        
+  
 
     def display(self):
- 
-
-        data1 = "Vehicle Odometer: " + str(self.odometerRead)
-        data2 = "Total Engine Hours: " + str(self.engineHours)
-        data3 = "Total Fuel Used: " + str(self.fuelUsed)
-        data4 = "Average Fuel Economy: " + str(self.fuelEconomy)
-        data5 = "Fuel Level 1: " + str(self.fuelLevel1)
-
-        totalData = data1 + '\n' + data2 + '\n' + data3 + '\n' + data4 + '\n' + data5
+        
+        Data = [str(self.serial_number), str(self.niin),str(self.tamcn),str(self.test),"","   Value:", "   " + str(self.odometerRead), "   " + str(self.engineHours), "   " + str(self.fuelUsed), "   " + str(self.fuelEconomy), "   " + str(self.fuelLevel1)]
+        Labels = ["Serial Number:","NIIN:","TAMCN:","ID Number:","","Parameter:","Vehicle Odometer:","Total Engine Hours:","Total Fuel Used:","Fuel Economy:","Fuel Level 1:"]
+        Units = ["","","","","","Units:","Miles","Hours","Gallons","Miles per Gallon","%"]
+        height = len(Data)
+        width = 3
 
         root = Tk()
-        root.geometry('250x150+1500+300')
-        label = Label(root, text= totalData, width=40, font=(None,15))
-        label.pack()
-
-
-
-
-
-
+        for i in range(height):
+            for j in range(width):
+                if j == 1:
+                    label = Label(root, text = Data[i],font=(None,15))
+                    label.grid(row=i,column =j,sticky = W)
+                elif j == 2:
+                    label = Label(root, text = Units[i],font=(None,15))
+                    label.grid(row=i,column =j,sticky = W)
+                else:
+                    label = Label(root, text = Labels[i],font=(None,15))
+                    label.grid(row=i,column =j,sticky = W) 
+        #height = 8
+        #width = 3
+        root.geometry('500x350+650+350')
+        #label = Label(root, text= totalData, width=150, font=(None,15))
+        #label.pack()
